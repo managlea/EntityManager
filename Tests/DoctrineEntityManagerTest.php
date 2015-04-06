2089b8608bdea8bea4e26fec94ed1d3908a82c51
Kauri Kont-Kontson <kaurikk@gmail.com>
2015-04-06 20:53:06 +0300

    Initial stuff

---
+++ Tests/DoctrineEntityManagerTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kauri
+ * Date: 06/04/15
+ * Time: 20:49
+ */
+
+namespace Managlea\Tests;
+
+
+class DoctrineEntityManagerTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @test
+     */
+    public function entityManager()
+    {
+        var_dump('foo');
+    }
+}
---
+++ src/EntityManager/DoctrineEntityManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Managlea\Component\EntityManager;
+
+
+use Managlea\Component\EntityManagerInterface;
+
+class DoctrineEntityManager implements EntityManagerInterface
+{
+    //
+}
