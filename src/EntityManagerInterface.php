944f8a558ce4f0603ee69ce31439faa4270d48f7
kaurikk <kaurikk@users.noreply.github.com>
2015-04-07 18:07:07 +0300

    Update EntityManagerInterface.php

--- src/EntityManagerInterface.php
+++ src/EntityManagerInterface.php
@@ -5,5 +5,11 @@ namespace Managlea\Component;
 
 interface EntityManagerInterface
 {
-    //
+    public function getRepository();
+    
+    public function persist();
+    
+    public function remove();
+    
+    public function flush();
 }
